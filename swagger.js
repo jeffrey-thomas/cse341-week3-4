2509f3d90be417fb05cc39e085ad156d6a62b25c
jeffrey-thomas <jeffrey.craig.thomas@gmail.com>
2025-03-29 16:43:10 -0700

    render host name

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: "Reading API",
     desccription: "CSE341 Project 2 - API to track books that readers have read",
   },
-  host: "localhost:3000",
-  schemes: ["http"],
+  host: "https://cse341-week3-4-3ob0.onrender.com/",
+  schemes: ["https"],
 };
 
 const ouput = "./swagger-out.json";
