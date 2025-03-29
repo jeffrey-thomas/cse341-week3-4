1845a4857c700e29b9d9982ef97a2eff4db50c7e
jeffrey-thomas <jeffrey.craig.thomas@gmail.com>
2025-03-29 16:55:38 -0700

    update swagger

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: "Reading API",
     desccription: "CSE341 Project 2 - API to track books that readers have read",
   },
-  host: "https://cse341-week3-4-3ob0.onrender.com",
+  host: "cse341-week3-4-3ob0.onrender.com",
   schemes: ["https"],
 };
 
